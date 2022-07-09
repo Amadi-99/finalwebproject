2ceb0aeae459ffd19ad1411bccfcf2c1a8757a64
Amadi-99 <96217@fhss.sjp.ac.lk>
2022-07-10 00:14:56 +0530

    add code for product list page

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>BOOKS</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled selected>
+              Order
+            </Option>
+            <Option>Popularity</Option>
+            <Option>Title A-Z</Option>
+            <Option>Title Z-A</Option>
+            <Option>Authors A-Z</Option>
+            <Option>Authors Z-A</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Arrivals
+            </Option>
+            <Option>Oldest</Option>
+            <Option>Newest</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
